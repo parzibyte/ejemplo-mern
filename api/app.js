d6f346178428087a4b7531ac3d97a96ee307579d
Luis Cabrera Benito <parzibyte@gmail.com>
2020-12-19 23:07:06 -0600

    CORS

--- api/app.js
+++ api/app.js
@@ -8,6 +8,13 @@ var enrutadorVideojuegos = require('./routes/videojuegos');
 
 var app = express();
 
+app.use((req, res, next) => {
+    res.set("Access-Control-Allow-Credentials", "true");
+    res.set("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.set("Access-Control-Allow-Headers", "Content-Type");
+    res.set("Access-Control-Allow-Methods", "OPTIONS,GET,PUT,POST,DELETE");
+    next();
+});
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

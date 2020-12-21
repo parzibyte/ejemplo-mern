911b8ea23b75ab75a7c30ba1ea2bef8732b8be88
Luis Cabrera Benito <parzibyte@gmail.com>
2020-12-21 15:55:50 -0600

    Cambios menores y elimnando archivos innecesarios

--- api/app.js
+++ api/app.js
@@ -3,7 +3,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
 var enrutadorVideojuegos = require('./routes/videojuegos');
 
 var app = express();
@@ -21,7 +20,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/videojuegos', enrutadorVideojuegos);
 
 module.exports = app;
